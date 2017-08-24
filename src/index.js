dea293e2531590b04be5ac2ff2abf777ff836452
Charles Kuo <CharlesK@ashn.com>
2017-08-24 16:40:25 -0700

    Add sourcemap debugger

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import './index.css'; // Webpack parse CSS then inject style sheet onto the page
 import numeral from 'numeral'; // library for formating number
 
 const courseValue = numeral(1000).format('$0,0.00');
+debugger;
 console.log(`I would pay ${courseValue} for this awsome course!`);
