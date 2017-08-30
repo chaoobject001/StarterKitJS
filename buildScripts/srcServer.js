ea19b37dfd0a1b493aed56454fd1b3b595048e91
Charles Kuo <chao.kuo@gmail.com>
2017-08-29 23:46:44 -0700

    Hard coded API and Centralized folder for api

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -26,6 +26,15 @@ app.get('/', function(req, res){
   // join the path to the source directory
 });
 
+app.get('/users', function(req, res){
+  // Hard coded for simplicity - it should have hit real database
+  res.json([
+    {"id":1, "firstName":"Bob", "lastName":"Smith", "email":"bob@gmail.com"},
+    {"id":2, "firstName":"Tammy", "lastName":"Norton", "email":"tnorton@yahoo.com"},
+    {"id":3, "firstName":"Tina", "lastName":"Lee", "email":"lee.tina@hotmail.com"}
+  ]);
+});
+
 // listen on the port defined above with express
 app.listen(port, function(err) {
   if (err) {
